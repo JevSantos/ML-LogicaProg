35f439abc6bfd992eaab350266a438937afb2c1f
jevdossantos <jeversos.dossantos@mercadolivre.com>
2025-06-03 15:16:43 -0300

    feat: melhoramentos

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int num2 = 0;
 
         do {
-            System.out.println("\n\t****** Benvindo a Calculadora Básica! ******");
+            System.out.println("\n\t****** Calculadora Básica! ******");
             System.out.println("\t 1 - Somar");
             System.out.println("\t 2 - Subtrair");
             System.out.println("\t 3 - Multiplicar");
@@ -34,19 +34,19 @@ public class Main {
                 num2 = sc.nextInt();}
 
             switch (escolha) {
-                case 1 -> System.out.print("A soma dos números é: " + somar(num1, num2));
-                case 2 -> System.out.print("A subtração dos números é: " + subtrair(num1, num2));
-                case 3 -> System.out.print("A multiplicação dos números é: " + multiplicar(num1, num2));
+                case 1 -> System.out.printf("\n A soma dos números é: %d \n", somar(num1, num2));
+                case 2 -> System.out.printf("\n A subtração dos números é: %d \n", subtrair(num1, num2));
+                case 3 -> System.out.printf("\n A multiplicação dos números é: %.02f \n", multiplicar(num1, num2));
                 case 4 -> {
                     if(num2 == 0) {
                         System.out.println("Não é possível dividir por zero!");
                     } else {
-                        System.out.print("A divisão dos números é: " + dividir(num1, num2));
+                        System.out.printf("\n A divisão dos números é: %.002f \n", dividir(num1, num2));
                     }
                 }
-                case 5 -> paruoimpar(num1);
-                case 6 -> System.out.print("O fatorial do número é: " + tirarfatorial(num1));
-                case 7 -> fazertabuada(num1);
+                case 5 -> parOuImpar(num1);
+                case 6 -> System.out.print("O fatorial do número é: " + calcularFatorial(num1));
+                case 7 -> calcularTabuada(num1);
                 case 8 -> System.out.println("Saindo...") ;
                 default -> System.out.println("Opção inválida! Tente novamente.");
             }
@@ -63,20 +63,19 @@ public class Main {
         return num1 * num2;
     }
     public static double dividir(double num1, double num2) {
-        double div = 0;
-        if (num2 != 0) {
-            div = num1 / num2;
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Não é possível dividir por zero.");
         }
-        return div;
+        return num1 / num2;
     }
-    public static void paruoimpar(int num1) {
+    public static void parOuImpar(int num1) {
         if (num1 % 2 == 0) {
             System.out.println("O número " + num1 + " É Par.");
         } else {
             System.out.println("O número " + num1 + " É Ímpar.");
         }
     }
-    public static long tirarfatorial(int num1) {
+    public static long calcularFatorial(int num1) {
         if (num1 < 0) {
             return -1; // Indica um erro
         } else if (num1 == 0) {
@@ -89,9 +88,16 @@ public class Main {
             return fatorial;
         }
     }
-    public static void fazertabuada(int num1){
+    public static void calcularTabuada(int num1){
         for(int i = 0; i <=10; i++){
             System.out.println("\t "+ num1 + " x "+ i +"= " + (num1 * i));
+            try {
+                Thread.sleep(350);
+            } catch (InterruptedException e) {
+                System.err.println("Cálculo interrompido");
+                Thread.currentThread().interrupt();
+                return;
+            }
         }
     }
 }
